e0ef45f377f531cef9916e4ec03928cac465f0ea
Zekaria Abdirahman Mohamed <zekariaam@student.uia.no>
2024-03-13 14:45:28 +0100

    :test_tube: Feat: add test for add button displays

--- bachCrudApp/BlazorTests/__test__/components/StuddentComponentButtns/AddButtonTest.cs
+++ bachCrudApp/BlazorTests/__test__/components/StuddentComponentButtns/AddButtonTest.cs
@@ -1,6 +1,31 @@
+using BachCrud.Client.Pages;
+using BlazorCrud.Client.Services;
+using BlazorCrud.Shared;
+using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
 namespace BlazorTests.__test__.components.StuddentComponentButtns;
 
-public class AddButtonTest
+public class AddButtonTest : TestContext
+
 {
+    [Fact]
+    public void AddButtonShouldDisplay()
+    {
+        var mockStudentService = new Mock<IStudentService>();
+
+        mockStudentService.Setup(s => s.AllStudents()).ReturnsAsync(new List<StudentDTO>());
+
+        Services.AddSingleton<IStudentService>(mockStudentService.Object);
+
+        var component = RenderComponent<Students>(); 
+        
+        var addNewStudentButton = component.Find("a.btn.btn-success");
+        
+        Assert.NotNull(addNewStudentButton); 
+        Assert.Equal("New Student", addNewStudentButton.TextContent.Trim());
+
+    }
     
 }

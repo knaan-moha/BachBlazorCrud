696c741555be01e70b96dff2671452eb6a00fc35
Zekaria Abdirahman Mohamed <zekariaam@student.uia.no>
2024-03-13 15:19:59 +0100

    :test_tube: Feat: add test for back button

--- bachCrudApp/BlazorTests/obj/Debug/net8.0/BlazorTests.AssemblyInfo.cs
+++ bachCrudApp/BlazorTests/obj/Debug/net8.0/BlazorTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f5f526da66d5ebc2a72945b6edaf30ab6bf95c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ef45f377f531cef9916e4ec03928cac465f0ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

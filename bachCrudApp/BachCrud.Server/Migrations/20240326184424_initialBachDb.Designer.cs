022e17c4449f84aa71ef61ad6af87f71f37feb6e
Zekaria Abdirahman Mohamed <zekariaam@student.uia.no>
2024-03-26 19:46:22 +0100

    :card_file_box: Feat: add new db connection string

--- bachCrudApp/BachCrud.Server/Migrations/20240326184424_initialBachDb.Designer.cs
+++ bachCrudApp/BachCrud.Server/Migrations/20240326184424_initialBachDb.Designer.cs
@@ -4,6 +4,7 @@ using BachCrud.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BachCrud.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplictionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240326184424_initialBachDb")]
+    partial class initialBachDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

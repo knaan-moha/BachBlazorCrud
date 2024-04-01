41cd1d3f713c937ac1d4038ed8bdbb766aa1d0a5
Zekaria Abdirahman Mohamed <zekariaam@student.uia.no>
2024-04-01 08:35:58 +0200

    :alien: Feat: update httpclient url

--- bachCrudApp/BachCrud.Client/obj/Debug/net8.0/BachCrud.Client.AssemblyInfo.cs
+++ bachCrudApp/BachCrud.Client/obj/Debug/net8.0/BachCrud.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BachCrud.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c12ac4301694eb014e26973d8536325682ae0db9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c71341c6f599f30ef6b4ddd7cabecbe9c10be8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BachCrud.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BachCrud.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- bachCrudApp/BachCrud.Shared/obj/Release/net7.0/BachCrud.Shared.AssemblyInfo.cs
+++ bachCrudApp/BachCrud.Shared/obj/Release/net7.0/BachCrud.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BachCrud.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8dbe34cb141e17668ff10ee2334d52bee8ece96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c71341c6f599f30ef6b4ddd7cabecbe9c10be8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BachCrud.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BachCrud.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
